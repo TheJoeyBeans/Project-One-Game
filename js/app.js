0254710a0da06ab4d305b9dab9166b840554ce88
Joseph Benbella <josephbenbella@Josephs-MacBook-Air.local>
2019-10-03 14:58:26 -0600

    Start and reset buttons are both set up

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@ const greenButton = document.getElementById('green');
 const redButton = document.getElementById('red');
 const yellowButton = document.getElementById('yellow');
 const blueButton = document.getElementById('blue');
+const startButton = document.getElementById('start');
+const resetButton = document.getElementById('reset');
+
+startButton.addEventListener('click', (e) =>{
+	console.log('buttonWorks');
+});
+
+resetButton.addEventListener('click', (e) =>{
+	console.log('buttonWorks');
+})
 
 greenButton.addEventListener('click', (e) =>{
 	console.log('buttonWorks');
